525547a468d4f1edf2293aba37ec62df0f53ebb5
Alexander Eger <40068802+3xz1@users.noreply.github.com>
2024-06-19 19:19:26 +0200

    Update CustomData.py

--- algos/CustomData.py
+++ algos/CustomData.py
@@ -2,6 +2,9 @@
 import os 
 
 print("Data Inputs: ", os.listdir('/data/inputs'))
+print("Files in / : ", os.listdir('/'))
+print("Files in /data : ", os.listdir('/data'))
+print("Files in /data/outputs : ", os.listdir('/data/outputs'))
 
 try:
   with open('/data/inputs/algoCustomData.json', 'r') as f:
